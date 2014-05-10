4a791a29780091f074f0a437c0320682a0657870
jaiwo99 <jaiwo99@gmail.com>
2014-05-10 22:41:09 +0200

    added default constructor for JiangPicking

--- src/main/java/com/jaiwo99/cards/domain/JiangPicking.java
+++ src/main/java/com/jaiwo99/cards/domain/JiangPicking.java
@@ -2,13 +2,19 @@ package com.jaiwo99.cards.domain;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serializable;
+
 /**
  * @author liang - jaiwo99@gmail.com
  */
 @Document(collection = "jiang_picking")
-public class JiangPicking extends Picking {
+public class JiangPicking extends Picking implements Serializable {
+
+    private static final long serialVersionUID = 2l;
 
     private String jiang;
+
+    public JiangPicking() {}
 
     public JiangPicking(String jiang) {
         this.jiang = jiang;
