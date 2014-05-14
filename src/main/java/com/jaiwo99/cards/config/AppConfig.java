1e6f83bb87336d60d46b18e274933f7f98ac93ce
jaiwo99 <jaiwo99@gmail.com>
2014-05-15 00:39:35 +0200

    clean up unnecessary deps.

--- src/main/java/com/jaiwo99/cards/config/AppConfig.java
+++ src/main/java/com/jaiwo99/cards/config/AppConfig.java
@@ -4,10 +4,7 @@ import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
 /**
  * @author liang - jaiwo99@gmail.com
@@ -17,35 +14,12 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 public class AppConfig {
 
     @Bean
-    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-        return new MappingJackson2HttpMessageConverter();
-    }
-
-    @Bean
-    public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
-        RequestMappingHandlerAdapter adapter = new RequestMappingHandlerAdapter();
-        adapter.afterPropertiesSet();
-        adapter.getMessageConverters().add(mappingJackson2HttpMessageConverter());
-        adapter.getMessageConverters().add(stringHttpMessageConverter());
-        return adapter;
-    }
-
-    @Bean
     public Gson gson() {
         return new Gson();
     }
 
     @Bean
     public RestTemplate restTemplate() {
-        final RestTemplate restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(mappingJackson2HttpMessageConverter());
-        restTemplate.getMessageConverters().add(stringHttpMessageConverter());
-
-        return restTemplate;
-    }
-
-    @Bean
-    public StringHttpMessageConverter stringHttpMessageConverter() {
-        return new StringHttpMessageConverter();
+        return new RestTemplate();
     }
 }
